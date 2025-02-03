67f8d02d2c1f4898b9d81a625612289606042e35
perassi <horacio2jp@gmail.com>
2025-02-03 10:44:21 -0800

    update untitled-ui

--- kiwiapp-public/src/app/(home)/page.tsx
+++ kiwiapp-public/src/app/(home)/page.tsx
@@ -1,3 +1,14 @@
+// import { Button } from "@headlessui/react";
+import { Button } from "@/components/common/Button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className=' flex flex-col gap-10'>
+      <span>Home</span>
+      <Button type='fill'>
+        <Link href='/enrollment'>Enrollment</Link>
+      </Button>
+    </div>
+  );
 }
